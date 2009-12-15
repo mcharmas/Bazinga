eec391a4b33d9706eee6766c47d9ffd4e9b11a9e
mcharmas <mcharmas@ffdda973-792b-4bce-9e62-2343ac01ffa1>
2009-12-15 18:24:01 +0000

    Poprawione dzialajace grupy z samousuwaniem
    git-svn-id: https://metrum.umcs.lublin.pl/svn/repos/bazinga@41 ffdda973-792b-4bce-9e62-2343ac01ffa1

--- app/serwer/src/Group.py
+++ app/serwer/src/Group.py
@@ -11,10 +11,11 @@ class Group():
     """
     def __init__(self, id=0):
         self.users = []
-        self.timeout = 500
+        self.timeout = 1
         self.id = id
         self.name = "Group("+str(id)+")"
-        self.timer = Timer(self.timeout, self.checkTimeouts)
+        self.timer = Timer(self.timeout, self.checkTimeouts)        
+        self.timer.start()
         
     def sendData(self,data):
         SLogger.log(self.name, "checkTimeouts()", "Wysylanie danych.")
@@ -23,12 +24,28 @@ class Group():
             
     def checkTimeouts(self):
         SLogger.log(self.name, "checkTimeouts()", "Sprawdzanie bezczynnosci.")
-        time = time.localtime()
-        for u in self.users:
-            if time.mktime(u.timestamp) - time.mktime(time) > self.timeout:
-                SLogger.log(self.name, "checkTimeouts()", "Usuwanie " + u.login)
-                del u
+        t = time.localtime()
+        for u in self.users:            
+            if  time.mktime(t) - time.mktime(u.timestamp) >= self.timeout:            
+                SLogger.log(self.name, "checkTimeouts()", "Usuwanie uzytkownika z powodu bezczynnosci " + u.login)
+                self.users.remove(u)
+        self.timer.cancel()
+        self.timer = Timer(self.timeout, self.checkTimeouts)
+        self.timer.start()
+               
                 
     def addUser(self, user):
         SLogger.log(self.name, "addUser()", "Dodawanie uzytkownika " + user.login)
         self.users.append(user)
+        
+if __name__ == "__main__":
+    g = Group()
+    u1 = User.User("dupa", "Jasio", g, ('localhost',1234), None)
+    u2 = User.User("dupa1", "Jasio1", g, ('localhost',1234), None)
+    u1.update()
+    g.addUser(u1)
+    time.sleep(5)    
+    u2.update()  
+    g.addUser(u2)
+    time.sleep(2)
+      
