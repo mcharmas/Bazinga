2e2c411fe7f3cdb73d6af8b2f6060a9a5f4e91e5
mcharmas <mcharmas@ffdda973-792b-4bce-9e62-2343ac01ffa1>
2009-12-15 17:56:07 +0000

    usprawnienia group
    git-svn-id: https://metrum.umcs.lublin.pl/svn/repos/bazinga@40 ffdda973-792b-4bce-9e62-2343ac01ffa1

--- app/serwer/src/Group.py
+++ app/serwer/src/Group.py
@@ -1,17 +1,34 @@
 import User
 import time
+from threading import Timer
+from Logger import SLogger
 
 class Group():
-    def __init__(self):
+    """
+    grupa przechowujaca userow
+    posiada timer sprawdzajacy co timeout bezczynnosc userow po ich timestampach
+    po bezczynnosci user jest usuwany (TODO: wysylac odnowienie polaczenia)
+    """
+    def __init__(self, id=0):
         self.users = []
         self.timeout = 500
+        self.id = id
+        self.name = "Group("+str(id)+")"
+        self.timer = Timer(self.timeout, self.checkTimeouts)
         
     def sendData(self,data):
+        SLogger.log(self.name, "checkTimeouts()", "Wysylanie danych.")
         for u in self.users:
             u.sendData(data)
             
     def checkTimeouts(self):
+        SLogger.log(self.name, "checkTimeouts()", "Sprawdzanie bezczynnosci.")
         time = time.localtime()
         for u in self.users:
             if time.mktime(u.timestamp) - time.mktime(time) > self.timeout:
+                SLogger.log(self.name, "checkTimeouts()", "Usuwanie " + u.login)
                 del u
+                
+    def addUser(self, user):
+        SLogger.log(self.name, "addUser()", "Dodawanie uzytkownika " + user.login)
+        self.users.append(user)
