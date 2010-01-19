800bdd6bc1a2cbabd7a669760be6ee832076cf43
rpycka <rpycka@ffdda973-792b-4bce-9e62-2343ac01ffa1>
2010-01-19 18:57:37 +0000

    jakas detekcja czworokatow, jeszcze daleko...
    git-svn-id: https://metrum.umcs.lublin.pl/svn/repos/bazinga@56 ffdda973-792b-4bce-9e62-2343ac01ffa1

--- app/CamDriver/quadrangles.h
+++ app/CamDriver/quadrangles.h
@@ -5,6 +5,9 @@
 
 class Quadrangles : public FrameRetreiver
 {
+private:
+    double angle( CvPoint* pt1, CvPoint* pt2, CvPoint* pt0 );
+
 public:
     Quadrangles(VideoInput* input);
     void retreiveFrame(cv::Mat &);
